ee77de0743c7612b4bad06501004c61bf8a1ab5c
Pedatela <lucaspedatela@gmail.com>
2021-08-09 12:43:58 -0300

    Ajuste

--- src/components/MovieRow/index.js
+++ src/components/MovieRow/index.js
@@ -7,7 +7,7 @@ import NavigateBeforeIcon from '@material-ui/icons/NavigateBefore';
 import NavigateNextIcon from '@material-ui/icons/NavigateNext';
 
 function MovieRow({ title, items }) {
-    const [scrollX, setScrollX] = useState(-400)
+    const [scrollX, setScrollX] = useState(0)
 
     const handleLeftArrow = () => {
         let x = scrollX + Math.round(window.innerWidth / 2)
